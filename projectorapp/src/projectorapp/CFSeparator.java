7d8af4481045c90832bd4ed3cef5013494835833
DBit46 <bittne12@students.rowan.edu>
2013-12-19 14:01:19 -0500

    Update CFSeparator.java

--- projectorapp/src/projectorapp/CFSeparator.java
+++ projectorapp/src/projectorapp/CFSeparator.java
@@ -1,5 +1,6 @@
 package projectorapp;
 
+import java.net.InetAddress;
 import java.util.StringTokenizer;
 
 /**
@@ -11,8 +12,63 @@ import java.util.StringTokenizer;
  * Peter Flanner, Dan Marzin, Josh Ogren, and Andrew Tasso.
  * 
  * @author marzin
+ * @author bittner
  * @version 0.1
  */
 public class CFSeparator {
+     StringTokenizer tokenizer;
+
+
+    public CFSeparator() {
+    }
     
+    public void constructConfig(String configString)
+    {
+     String title = "";
+     String type = "";
+     String make = "";
+     String model = "";
+     InetAddress ipAddress;
+     int speed = 0;
+     int dataBits = 0;
+     String parity = "";
+     int stopBits = 0;
+     boolean tecRoom = false;
+     String pc = "";
+     String laptop = "";
+     String dvd = "";
+     String vcr = "";
+     String doccam = "";
+      
+      tokenizer = new StringTokenizer(configString,"\n", false);
+      while(tokenizer.hasMoreTokens())
+      {
+        String token = tokenizer.nextToken();
+        StringTokenizer lineTokenizer = new StringTokenizer(token, "=", false);
+        String option = lineTokenizer.nextToken().trim().toUpperCase();
+        if(option.equals("NAME"))
+        {   title = lineTokenizer.nextToken();    
+        }
+        else if(option.equals("TYPE"))
+        {   type = lineTokenizer.nextToken();}
+        else if(option.equals("MAKE"))
+        {   make = lineTokenizer.nextToken();}
+        else if(option.equals("MODEL"))
+        {   model = lineTokenizer.nextToken();} 
+        else if(option.equals("IPADDRESS"))
+        {}
+         else if(option.equals("SERIAL"))
+        {   StringTokenizer subTokenizer = new StringTokenizer(lineTokenizer.nextToken(), " ", false);
+         try {    
+            speed = Integer.parseInt(subTokenizer.nextToken());
+            dataBits = Integer.parseInt(subTokenizer.nextToken());
+            parity = subTokenizer.nextToken();
+            stopBits = Integer.parseInt(subTokenizer.nextToken());
+             }
+         catch (NumberFormatException e)
+         {System.out.println("Serial field had value that was not an integer that should have been");}
+        }
+      }
+    
+    }
 }
